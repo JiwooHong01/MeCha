c037662dd99ba9b9b9b7f10c759a4948236dd1ed
JiwooHong01 <jiwooinab@gmail.com>
2025-03-14 15:43:42 +0900

    kor->eng->kor

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pprint
 from langchain_core.messages import BaseMessage, HumanMessage
-from translator import translate_korean_to_english
+from translator import *
 
 def user(graph):
     print("질문을 입력하면 RAG 챗봇이 답변합니다. (종료하려면 exit)")
@@ -11,7 +11,7 @@ def user(graph):
             break
 
         # korean_input = "안녕하세요? 저는 한국어를 영어로 번역하는 모델입니다."
-        # user_input = translate_korean_to_english(user_input)
+        user_input = translate_korean_to_english(user_input)
         # print("번역 결과:", english_output)
 
         inputs = {
@@ -26,6 +26,7 @@ def user(graph):
             for key, value in output.items():
                 pprint.pprint(f"Output from node '{key}':")
                 pprint.pprint("---")
+                value = translate_english_to_korean(value)
                 pprint.pprint(value, indent=2, width=80, depth=None)
             pprint.pprint("\n---\n")
     # inputs = {
--- translator.py
+++ translator.py
@@ -5,6 +5,30 @@ import sys
 from dotenv import load_dotenv
 
 # 발급받은 OpenAI API 키를 설정하세요.
+def translate_english_to_korean(english_text: str)-> str:
+    load_dotenv()
+    _set_env("OPENAI_API_KEY")
+    client = OpenAI()
+    # ChatGPT에게 한국어 -> 영어 번역을 명시적으로 요청하기 위해 system/message를 구성합니다.
+    # temperature=0.0으로 설정하면 결과가 더 일관적입니다.
+    response = client.chat.completions.create(
+        model="gpt-4",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful translator who accurately translates English text into Korean. First: Detect the language of the input text Second: Translate the input text from the detected language in first step to the English language Third: Return output only the translated text not add up the description."
+            },
+            {
+                "role": "user",
+                "content": f"Please translate the following English text into Korean:\n'{english_text}'"
+            }
+        ],
+        temperature=0.0
+    )
+    
+    # API의 응답에서 번역된 텍스트를 추출
+    translated_text = response.choices[0].message.content
+    return translated_text
 
 def translate_korean_to_english(korean_text: str) -> str:
     load_dotenv()
